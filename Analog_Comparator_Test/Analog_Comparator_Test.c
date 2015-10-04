971d63c86df07f0804f63433e646b370adeb382c
ryood <kawanabe@gmail.com>
2015-10-04 20:43:33 +0900

    impl analog comparator

--- Analog_Comparator_Test/Analog_Comparator_Test.c
+++ Analog_Comparator_Test/Analog_Comparator_Test.c
@@ -7,6 +7,10 @@
  * ATiny13
  * Fuse l:7Ah h:FFh
  *
+ * PB0: AIN1 コンパレータ―反転入力
+ * PB4: LED
+ * 
+ * 2015.10.04 Tiny13コンパレータテスト
  * 2015.10.04 Tiny13のLチカテスト
  *
  */ 
@@ -14,17 +18,41 @@
 #define F_CPU 9600000
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+
+ISR (ANA_COMP_vect)
+{
+	if (ACSR & (1 << ACO))
+	{
+		PORTB |= (1 << PB4);
+	}
+	else
+	{
+		PORTB &= ~(1 << PB4);
+	}
+}
 
 int main(void)
 {
-	DDRB = 0x01;
+	DDRB = 0x00;
+		
+	// LED output
+	DDRB |= (1 << PB4);
+	
+	// AIN1をバンドギャップ・リファレンス、割込み有効化
+	ACSR = (1 << ACBG) | (1 << ACIE);
+	
+	sei();
 	
     while(1)
-    {
-        PORTB |= (1 << PB0);
+    {	
+		/*	
+		PORTB |= (1 << PB4);
 		_delay_ms(500);
-		PORTB &= ~(1 << PB0);
+		PORTB &= ~(1 << PB4);
 		_delay_ms(500);
+		*/
+		
     }
 }
